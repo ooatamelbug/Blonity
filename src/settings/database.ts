0e1f60ca5674303a3b978e2c075a9065b7c19f12
ooatamelbug <mohamedwork636@gmail.com>
2021-12-02 19:48:41 +0200

    edit in database connection

--- src/settings/database.ts
+++ src/settings/database.ts
@@ -1,18 +1,27 @@
+// import createConnection from typeorm
 import { createConnection } from "typeorm";
 
+/**
+ * create function databaseConnection 
+ * it return complete create connection
+ */
 const databaseConnection = async () => {
   try {
+    // await to createConnection with db
     await createConnection({
       database: "blonity",
       password: "postgres",
+      username: "postgres",
       type: "postgres",
-      port: 5433,
+      port: 5432,
       logging: true,
-      entities: ["../entities/*.ts"],
+      entities: ["../entities/**.ts"],
       synchronize: true,
     });
+    // log 
     console.log("Connection created");
   } catch (error) {
+      // return error if connection get error
       throw error;
   }
 };
