46ade32553f4eb7db69032ff85ccc2d573aca350
ooatamelbug <mohamedwork636@gmail.com>
2021-12-08 17:51:04 +0200

    edit in test user

--- src/test/User.test.ts
+++ src/test/User.test.ts
@@ -1,4 +1,4 @@
-import { ResponseDataInterface } from "../settings/interfaces";
+import "jest";
 import app from "../app";
 import supertest from "supertest";
 import databaseConnection from "../settings/database";
@@ -11,22 +11,21 @@ beforeAll(async () => {
 });
 
 describe("test user service api", () => {
-  it("test user if empty value", async () => {
-      const res = await request
-      .post("/api/user/create")
-      .send({
-        username: "",
-      });
-      expect(res.statusCode).toBe(400);
-      expect(res.body)
+  it("test username if empty value to test middleware object return", async () => {
+    const res = await request.post("/api/user/create").send({
+      username: "",
+    });
+    expect(res.statusCode).toBe(400);
+    expect(res.body).not.toBeNull();
+    expect(res.body).toHaveProperty("message");
+    expect(res.body.errors).toHaveLength(1);
+    expect(res.body.inputValidation).not.toBeNull();
   });
 
   it("test user return object", async () => {
-      const res = await request
-        .post("/api/user/create")
-        .send({
-          username: "name",
-        });
-        expect(res.statusCode).toEqual(200);
+    const res = await request.post("/api/user/create").send({
+      username: "name",
+    });
+    expect(res.statusCode).toEqual(200);
   });
 });
